515d96c4a1350823c73df8039daaf5aa8b295df3
Adam29C <adamcatholic29@gmail.com>
2025-04-29 11:40:21 +0530

    server code uplode

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,29 @@
 // })
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src')
+//     }
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import fs from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+  server: {
+    host: '0.0.0.0', // All interfaces
+    port: 5173,
+    allowedHosts: [
+      'chatit.minidog.club', // Add your domain here
+    ],
   },
-});
+})
+
