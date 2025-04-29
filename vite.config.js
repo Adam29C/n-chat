294f2c2acffcbcd673ac5a2d3762374203caef29
Adam29C <adamcatholic29@gmail.com>
2025-04-29 12:06:21 +0530

    update build

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,23 @@
 //   plugins: [react()],
 // })
 
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0', // All interfaces
+    port: 5173,
+    allowedHosts: [
+      'chatit.minidog.club', // Add your domain here
+    ],
   },
 });
